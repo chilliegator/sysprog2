c0f4b045b58fc2648f032dd6d94cfe8cc962aec2
chilliegator <149959312+chilliegator@users.noreply.github.com>
2025-04-21 18:39:01 +0300

    Update help.c

--- src/help.c
+++ src/help.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-#include "../include/help.h"
 
+// Функция для вывода справки по использованию программы
 void print_help() {
-    printf("Usage: program [options]\n");
+    printf("Usage: program [option]\n");
     printf("Options:\n");
-    printf("  -u, --users       Print list of users and their home directories\n");
-    printf("  -p, --processes   Print list of running processes sorted by PID\n");
-    printf("  -h, --help        Print this help message\n");
-    printf("  -l PATH, --log PATH   Redirect output to the specified file\n");
-    printf("  -e PATH, --errors PATH   Redirect errors to the specified file\n");
+    printf("	-u, --users	Print list of users and their home directories\n");
+    printf("	-p, --processes	Print list of running processes\n");
+    printf("	-h, --help	Print this help message\n");
+    printf("	-l PATH, --log PATH	Redirect output to the specified file\n");
+    printf("	-e PATH, --errors PATH	Redirect errors to the specified file\n");
 }
