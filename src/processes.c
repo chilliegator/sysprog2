50e1945b9dbe69ba9078df925f4e2313c935c729
chilliegator <149959312+chilliegator@users.noreply.github.com>
2025-04-21 18:40:50 +0300

    Update processes.c

--- src/processes.c
+++ src/processes.c
@@ -1,28 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <dirent.h>
-#include <ctype.h>
-#include "../include/processes.h"
+#include <dirent.h>  // Для работы с директориями
 
-void print_processes_sorted() {
-    DIR *dir;
-    struct dirent *entry;
+// Функция для вывода списка процессов
+void print_processes() {
+    DIR *dir;  // Указатель на директорию
+    struct dirent *entry;  // Структура для хранения информации о файле/директории
 
-    // /proc path
+    // Открываем директорию /proc, где хранится информация о процессах
     dir = opendir("/proc");
     if (dir == NULL) {
+        // Если открыть директорию не удалось, выводим ошибку и завершаем функцию
         perror("opendir");
         return;
     }
 
-    // reading dir
+    // Читаем записи в директории
     while ((entry = readdir(dir)) != NULL) {
-        // checking PID
-        if (entry->d_type == DT_DIR && isdigit(entry->d_name[0])) {
-            printf("PID: %s\n", entry->d_name);  // PID printing
+        // Проверяем, является ли запись директорией (процессом)
+        if (entry->d_type == DT_DIR) {
+            // Выводим PID процесса
+            printf("Process ID: %s\n", entry->d_name);
         }
     }
 
-    // closing dir
+    // Закрываем директорию
     closedir(dir);
 }
