fc48d7c3a615aac733ecacfd8007dffabc6934f4
chilliegator <149959312+chilliegator@users.noreply.github.com>
2025-04-21 18:40:26 +0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,46 +1,78 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include "../include/users.h"
-#include "../include/processes.h"
-#include "../include/log.h"
-#include "../include/errors.h"
-#include "../include/help.h"
+#include <unistd.h>  // Для getopt()
+#include "users.h"
+#include "processes.h"
+#include "help.h"
+#include "log.h"
+#include "errors.h"
+
+// Функция для симуляции ошибки (открытие несуществующего файла)
+//void simulate_error() {
+//   FILE *file = fopen("Non_existent_file.txt", "r");
+//    if (file == NULL) {
+        // Если файл не удалось открыть, выводим ошибку в stderr
+//        perror("Error: Failed to open file"); // Искусственно созданная ошибка
+//    }
+//}
 
 int main(int argc, char *argv[]) {
     int opt;
-    char *log_path = NULL;  // path for stdout redirecting
-    char *errors_path = NULL;  // path for stderr redirecting
+    const char *log_path = NULL;  // Путь для перенаправления stdout
+    const char *errors_path = NULL;  // Путь для перенаправления stderr
+    int users_flag = 0;  // Флаг для вывода списка пользователей
+    int processes_flag = 0;  // Флаг для вывода списка процессов
 
+    // Обработка аргументов командной строки с помощью getopt
     while ((opt = getopt(argc, argv, "uphl:e:")) != -1) {
         switch (opt) {
             case 'u':
-                if (log_path) {
-                    redirect_output_to_file(log_path);  // Перенаправляем stdout в файл
-                }
-                print_users_sorted();  // users list printing
+                users_flag = 1;  // Устанавливаем флаг для вывода пользователей
                 break;
             case 'p':
-                if (log_path) {
-                    redirect_output_to_file(log_path);  // stdout redirecting
-                }
-                print_processes_sorted();  // proccesses list printing
+                processes_flag = 1;  // Устанавливаем флаг для вывода процессов
                 break;
             case 'h':
-                print_help();  // help manual printing
+                print_help();  // Вывод справки
                 return 0;
             case 'l':
-                log_path = optarg;  //  stdout path saving
+                log_path = optarg;  // Сохраняем путь для логов
                 break;
             case 'e':
-                errors_path = optarg;  // stderr path saving
-                redirect_errors_to_file(errors_path);  // stderr redirecting
+                errors_path = optarg;  // Сохраняем путь для ошибок
                 break;
             default:
+                // Если аргумент неизвестен, выводим сообщение об ошибке и справку
                 fprintf(stderr, "Unknown option: %c\n", opt);
                 print_help();
-                return 1;
+                return EXIT_FAILURE;
         }
+    }
+
+    // Перенаправляем stdout в файл, если указан флаг -l
+    if (log_path) {
+        redirect_output(log_path);
+    }
+
+    // Перенаправляем stderr в файл, если указан флаг -e
+    if (errors_path) {
+        redirect_errors(errors_path);
+    }
+
+    // Симулируем ошибку (открытие несуществующего файла)
+//    simulate_error();
+
+    // Выполняем команды в зависимости от флагов
+    if (users_flag) {
+        print_users();  // Вывод списка пользователей
+    }
+    if (processes_flag) {
+        print_processes();  // Вывод списка процессов
+    }
+
+    // Если не было указано ни одной команды, выводим справку
+    if (optind == 1 && !users_flag && !processes_flag) {
+        print_help();
     }
 
     return 0;
